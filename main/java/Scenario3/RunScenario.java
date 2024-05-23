31698949c3578a1da07871f5f4f678cf5a351b99
yuh2017 <34501842+yuh2017@users.noreply.github.com>
2024-05-23 16:03:06 +0200

    Add files via upload

---
+++ main/java/Scenario3/RunScenario.java
@@ -0,0 +1,13 @@
+package Scenario3;
+
+public class RunScenario {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        //new String[]{ "examples/tutorial/config/example5-config.xml" }
+        RunFromConfigfile.main(args) ;
+    }
+
+}
+
+/* if network capacity is 0, then results legs will have no data */
